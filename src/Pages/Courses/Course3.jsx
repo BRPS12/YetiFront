8de71b2a20d18539f720b7ea539b392d18b799aa
BRPS12 <boochkoob@gmail.com>
2024-07-26 11:44:16 +0800

    s

--- src/Pages/Courses/Course3.jsx
+++ src/Pages/Courses/Course3.jsx
@@ -3,6 +3,7 @@ import { Header } from "../../components/Header";
 import courseImage from "../../Images/Computer.jpg";
 import { Link } from "react-router-dom";
 import { Footer } from "../../components/Footer";
+import "./Course3.css";
 
 export const Course3 = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -12,7 +13,7 @@ export const Course3 = () => {
   }, []);
 
   return (
-    <div style={{ fontFamily: "Georgia, sans-serif" }}>
+    <div className="course-container">
       <Header
         backgroundImage={courseImage}
         title="Ерөнхий англи хэл"
@@ -20,105 +21,30 @@ export const Course3 = () => {
         repeat="no-repeat"
         bgSize="cover"
       />
-      <div
-        style={{
-          padding: "20px",
-          backgroundColor : "#1FD0FC",
-          paddingBottom: "100px",
-        }}>
-        <div style={{ marginBottom: "20px", width: "90vw" }}>
-          <p
-            style={{
-              fontSize: "30px",
-              color: "#fff",
-              fontFamily: "Georgia",
-              marginLeft: "40px",
-            }}>
-            Америкийн ЕБС-н хичээлийн хөтөлбөр+ IELTS, TOEFL суурь
-          </p>
+      <div className="course-content">
+        <div className="course-title">
+          <p>Америкийн ЕБС-н хичээлийн хөтөлбөр+ IELTS, TOEFL суурь</p>
         </div>
-        <div style={{ flexDirection: "row", display: "flex" }}>
-          <div
-            style={{
-              marginBottom: "20px",
-              borderWidth: "1px",
-              borderRadius: "40px",
-              borderColor: "#000",
-              borderStyle: "solid",
-              width: "50vw",
-              fontWeight: "lighter",
-              marginLeft: "30px",
-              background: "linear-gradient(#FFE1F2 , #fff)",
-              boxShadow: "1px 4px 6px #555",
-              marginRight: "30px",
-            }}>
-            <p
-              style={{
-                fontSize: "24px",
-                lineHeight: "1.6",
-                padding: "30px",
-                marginLeft: "20px",
-                marginTop: "-10px",
-                marginBottom: "-5px",
-              }}>
-              <p>
-                👏Ерөнхий англи хэлний анги нь 7 хоногт 3 удаа хичээллэнэ.
-                Хичээл нь Мягмар, Пүрэв болон Бямба гаригуудад 10:00-11:30,
-                14:00-15:30 цагийн хооронд. Ерөнхий англи хэлний ангид
-                бүртгүүлснээр:
-              </p>
-              <p> 1. Сар бүр сурагчид жишиг тест хийж ахицаа харах</p>
-              <p> 2. 16 жилийн туршлагатай багш заана</p>
-              <p>
-                3. Сар болгон сурагчийн сурлагын тайланг асран хамгаалагчид
-                мэйлээр илгээх
-              </p>
+        <div className="course-info">
+          <div className="info-box" style={{marginTop : "2vh" , marginLeft : 0}}>
+            <p style={{marginTop : "3vh"}}>
+              👏Ерөнхий англи хэлний анги нь 7 хоногт 3 удаа хичээллэнэ.
+              Хичээл нь Мягмар, Пүрэв болон Бямба гаригуудад 10:00-11:30,
+              14:00-15:30 цагийн хооронд. Ерөнхий англи хэлний ангид
+              бүртгүүлснээр:
+            </p>
+            <p> 1. Сар бүр сурагчид жишиг тест хийж ахицаа харах</p>
+            <p> 2. 16 жилийн туршлагатай багш заана</p>
+            <p style={{ marginBottom : "3vh"}}>
+              3. Сар болгон сурагчийн сурлагын тайланг асран хамгаалагчид
+              мэйлээр илгээх
             </p>
           </div>
-          {/* <div>
-            <p style={{ fontSize: "48px", marginTop: "70px" }}>😎</p>
-            <p style={{ fontSize: "48px", marginTop: "150px" }}>😉</p>
-          </div> */}
-          <div
-            style={{
-              marginBottom: "20px",
-              borderWidth: "1px",
-              borderRadius: "40px",
-              borderColor: "#000",
-              borderStyle: "solid",
-              width: "50vw",
-              fontWeight: "lighter",
-              marginLeft: "30px",
-              background: "linear-gradient(#fff ,#fff ,  #FFE1F2)",
-              boxShadow: "1px 4px 6px #555",
-              marginRight: "30px",
-            }}>
-            {/* <img src={require("../Images/Shureebagsh5.png")} style={{marginTop : "30px" , marginLeft : "20px"}}/> */}
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "row",
-                alignItems: "center",
-                justifyContent: "center",
-              }}>
-              <p
-                style={{
-                  fontSize: "27px",
-                  fontFamily: "monospace",
-                  marginTop: "40px",
-                  color: "Red",
-                  fontWeight: "bold",
-                }}>
-                Youth Educational Training Academy
-              </p>
+          <div className="info-box" style={{marginTop : "2vh" , marginBottom : "2vh"}}>
+            <div className=" academy-info">
+              <p>Youth Educational Training Academy</p>
             </div>
-            <p
-              style={{
-                fontSize: "24px",
-                lineHeight: "1.6",
-                padding: "30px",
-                marginLeft: "20px",
-              }}>
+            <p>
               {" "}
               <p>4. Цахимаар хичээлд суух </p>
               <p>5. Давтлагад суух</p> <p>6. Спорт зааланд тоглох </p>
