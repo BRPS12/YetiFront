da623e4e48acbb3ded9140d1ba302e4547a97d54
BRPS12 <boochkoob@gmail.com>
2024-07-28 00:27:45 +0800

    About done

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -131,6 +131,13 @@ export const About = () => {
 </div>
 
 <div class="experience-section">
+<div class="image-container">
+    <img
+      alt=""
+      src={require("../Images/ShureeBagsh.png")}
+      class="shureeBagshImage1"
+    />
+  </div>
   <div class="text-container">
     <div class="experience-block">
       <p class="year">2021 он</p>
@@ -140,9 +147,9 @@ export const About = () => {
         хүртэл англи хэлний цахим хичээлийг ямар ч үнэ төлбөргүйгээр
         зохион байгуулсан.
       </p>
-      <hr class="divider"></hr>
+      {/* <hr class="divider"></hr> */}
     </div>
-
+{/* 
     <div class="experience-block">
       <p class="year">2021 он</p>
       <p class="descriptionofExp">
@@ -151,149 +158,94 @@ export const About = () => {
         хүртэл англи хэлний цахим хичээлийг ямар ч үнэ төлбөргүйгээр
         зохион байгуулсан.
       </p>
-    </div>
-  </div>
-  <div class="image-container">
-    <img
-      alt=""
-      src={require("../Images/ShureeBagsh.png")}
-      class="shureeBagshImage1"
-    />
+    </div> */}
   </div>
 </div>
-      <div
-        style={{
-          width: "100%",
-          height: "100vh",
-          fontFamily: "Georgia, serif",
-        }}>
-        <hr></hr>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
-          }}>
-          <div style={{ marginLeft: "70px" }}>
-            <p style={{ fontSize: "40px" }}>2021 он</p>
-            <p style={{ width: "90%", fontSize: "25px", marginTop: "2vh" }}>
-              5 сарын 1 нээс 6 сарын 1 хүртэл 5-21 насны хүүхэд залуусын дунд
-              “Миний дуртай дүр”, “Миний дуртай дүр” эсээний уралдаан зохион
-              байгуулж буй нийт бүх тэмцээний эхний 3 байрны ялагчид 3-10 сар
-              үргэлжлэх сургалтанд суух эрх болон гарын бэлгээр шагнагдсан.
-            </p>
-          </div>
-          <div
-            style={{
-              marginLeft: "auto",
-              marginRight: "30px",
-              marginTop: "20px",
-              marginBottom: "20px",
-            }}>
-            <img
-              alt=""
-              src={require("../Images/information1.png")}
-              style={{ borderRadius: "20px" }}
-            />
-          </div>
-        </div>
-        <hr></hr>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
-          }}>
-          <div
-            style={{
-              marginLeft: "50px",
-              marginTop: "15px",
-              marginBottom: "15px",
-              marginRight: "auto",
-            }}>
-            <img
-              alt=""
-              src={require("../Images/information2.png")}
-              style={{ borderRadius: "20px" }}
-            />
-          </div>
-          <div style={{ marginLeft: "100px" }}>
-            <p style={{ fontSize: "40px" }}>2021 он</p>
-            <p style={{ width: "90%", fontSize: "25px", marginTop: "2vh" }}>
-              6 сарын 15 - 8 сарын 15-ы хооронд Монгол анх удаа “Мянган
-              инээмсэглэл“ нэртэйгээр нийт 1000 хүүхдийг TOEFL ITP, Ерөнхий
-              англи хэл, Математик, Физик, Хятад хэл ,Түрк хэл, Уран зураг ,
-              Балет , Гитар, Калиграф болон Видео засварлах гэсэн хичээлүүдийг
-              зохион байгуулж боловсролын салбарт 50,000,000 төгрөгний хөрөнгө
-              оруулалтыг хийсэн.
-            </p>
-          </div>
-        </div>
-        <hr></hr>
-      </div>
-      <div
-        style={{ width: "100%", height: "auto", fontFamily: "Georgia, serif" }}>
-        <div style={{ marginTop: "30px" }}>
-          <p style={{ fontSize: "40px", textAlign: "center" }}>2021 он</p>
-          <p
-            style={{
-              fontSize: "25px",
-              textAlign: "center",
-              marginTop: "10px",
-            }}>
-            Монгол даяар нийт 12 аймгийн 45 багшийг “ Үндэсний сургагч багш “
-            TOEFL ITP шалгалт авах, заах эрх өгөх, бэлтгэх сургалтыг зохион
-            байгуулж үүнээс 12 багш олон улсын шалгалт авах төвийг нээн хамтарч
-            ажиллах төслийг хэрэгжүүлэн үргэлжлүүлэн ажиллаж байна.
-          </p>
-        </div>
-        <div
-          style={{
-            marginTop: "20px",
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
-            justifyContent: "center",
-          }}>
-          <img
-            alt=""
-            src={require("../Images/duu1.jpg")}
-            style={{ borderRadius: "20px", width: "300px" }}
-          />
-          <img
-            alt=""
-            src={require("../Images/duu2.jpg")}
-            style={{ borderRadius: "20px", width: "300px", marginLeft: "50px" }}
-          />
-          <img
-            alt=""
-            src={require("../Images/duu3.jpg")}
-            style={{ borderRadius: "20px", width: "300px", marginLeft: "50px" }}
-          />
-        </div>
-        <div style={{ paddingBottom: "70px" }}>
-          <p
-            style={{
-              fontSize: "40px",
-              textAlign: "center",
-              marginTop: "20px",
-            }}>
-            2022 он
-          </p>
-          <p
-            style={{
-              fontSize: "25px",
-              textAlign: "center",
-              marginTop: "15px",
-            }}>
-            “Англи дууг хэн сайн дуулах вэ” Cover дууны уралдаан заралж эхний 3
-            байрандаа сургалтанд суух 1 жилийн эрх, 1.000.000, 500.000 ба
-            300.000 төгрөгний хадгаламж нээх эрх олгосон. Мөн шилдэг 15
-            дуучиддаа сарын сургалтанд суух эрхээр шагнасан /Мидвэй Эвэнт төв
-            үйл ажиллагааны зардал 15.000.000-г ивээн тэтгэсэн
-          </p>
-        </div>
-      </div>
+
+
+<div className="songofabout-container">
+  <hr />
+  <div className="songofabout-row">
+    <div className="songofabout-col-left">
+      <p className="songofabout-year">2021 он</p>
+      <p className="songofabout-description">
+        5 сарын 1 нээс 6 сарын 1 хүртэл 5-21 насны хүүхэд залуусын дунд
+        “Миний дуртай дүр”, “Миний дуртай дүр” эсээний уралдаан зохион
+        байгуулж буй нийт бүх тэмцээний эхний 3 байрны ялагчид 3-10 сар
+        үргэлжлэх сургалтанд суух эрх болон гарын бэлгээр шагнагдсан.
+      </p>
+    </div>
+    <div className="songofabout-col-right">
+      <img
+        alt=""
+        src={require("../Images/information1.png")}
+        className="songofabout-image"
+      />
+    </div>
+  </div>
+  <hr />
+  <div className="songofabout-row2">
+    <div className="songofabout-col-lef2">
+      <img
+        alt=""
+        src={require("../Images/information2.png")}
+        className="songofabout-image2"
+      />
+    </div>
+    <div className="songofabout-col-right2">
+      <p className="songofabout-year2">2021 он</p>
+      <p className="songofabout-description2">
+        6 сарын 15 - 8 сарын 15-ы хооронд Монгол анх удаа “Мянган
+        инээмсэглэл“ нэртэйгээр нийт 1000 хүүхдийг TOEFL ITP, Ерөнхий
+        англи хэл, Математик, Физик, Хятад хэл ,Түрк хэл, Уран зураг ,
+        Балет , Гитар, Калиграф болон Видео засварлах гэсэн хичээлүүдийг
+        зохион байгуулж боловсролын салбарт 50,000,000 төгрөгний хөрөнгө
+        оруулалтыг хийсэн.
+      </p>
+    </div>
+  </div>
+  <hr />
+</div>
+
+
+<div className="compitition-container">
+  <div className="compitition-header">
+    <p className="compitition-year">2021 он</p>
+    <p className="compitition-description">
+      Монгол даяар нийт 12 аймгийн 45 багшийг “ Үндэсний сургагч багш “
+      TOEFL ITP шалгалт авах, заах эрх өгөх, бэлтгэх сургалтыг зохион
+      байгуулж үүнээс 12 багш олон улсын шалгалт авах төвийг нээн хамтарч
+      ажиллах төслийг хэрэгжүүлэн үргэлжлүүлэн ажиллаж байна.
+    </p>
+  </div>
+  <div className="compitition-images">
+    <img
+      alt=""
+      src={require("../Images/duu1.jpg")}
+      className="compitition-image"
+    />
+    <img
+      alt=""
+      src={require("../Images/duu2.jpg")}
+      className="compitition-image"
+    />
+    <img
+      alt=""
+      src={require("../Images/duu3.jpg")}
+      className="compitition-image"
+    />
+  </div>
+  <div className="compitition-footer">
+    <p className="compitition-year">2022 он</p>
+    <p className="compitition-description">
+      “Англи дууг хэн сайн дуулах вэ” Cover дууны уралдаан заралж эхний 3
+      байрандаа сургалтанд суух 1 жилийн эрх, 1.000.000, 500.000 ба
+      300.000 төгрөгний хадгаламж нээх эрх олгосон. Мөн шилдэг 15
+      дуучиддаа сарын сургалтанд суух эрхээр шагнасан /Мидвэй Эвэнт төв
+      үйл ажиллагааны зардал 15.000.000-г ивээн тэтгэсэн
+    </p>
+  </div>
+</div>
       <Footer />
     </div>
   );
