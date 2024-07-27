6569276051cdd5d1b03f3c688aa0c90e77bb15b6
BRPS12 <boochkoob@gmail.com>
2024-07-27 13:23:35 +0800

    s

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ export const App = () => {
   }, []);
 
   return (
-    <div style={{width : "100%" , height : "100vh" , padding : 0 , margin : 0}}>
+    <div style={{padding : 0 , margin : 0}}>
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Home />} />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -51,10 +51,6 @@ export const Header = ({
       style={{
         padding: 0,
         margin: 0,
-        width: "100%",
-        height: "100vh",
-        position: "relative",
-        overflow: "hidden",
       }}>
       <header>
         <div
